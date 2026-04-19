baf498691f3dbc3b0a73b75f4f218dc0a7f4e3ee
Karl <Karlonugha@yahoo.com>
2026-04-19 14:15:16 +0100

    Fix SSL: add checkServerIdentity override for RDS

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,13 @@
 const { Pool } = require('pg');
 const { DATABASE_URL } = require('./env');
 
+// Parse the connection string manually to allow SSL override
 const pool = new Pool({
   connectionString: DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+    checkServerIdentity: () => undefined,
+  },
 });
 
 module.exports = pool;
